bdc17ce8fc84462997eba4cc70b50721d22ac18c
princemiphetattamongkhon@gmail.com <princemiphetattamongkhon@gmail.com>
2026-05-11 23:39:07 +0700

    yotube music bill board

--- musical_game/main.py
+++ musical_game/main.py
@@ -16,13 +16,39 @@ for tag in all_song_tag:
     song_name = tag.getText()
     song_name = song_name.strip()
     song_names.append(song_name)
-# print(song_names)
-yt = YTMusic(r"c:\game\musical_game\browser.json")
-playlists = yt.get_library_playlists()
-print(f"Found {len(playlists)} playlists in your library.")
+print(song_names)
 
-## will fix tommorow 
-playlistId = YTMusic.create_playlist(
-    "test",
-    "test description"
-)
+# YouTube Music Authentication
+yt = YTMusic(r"c:\game\musical_game\browser.json")
+
+# Create playlist
+Player_Name = f"{date} Bill Board 100"
+playlist_id = None
+playlists = yt.get_library_playlists()
+
+for i in playlists:
+    if i["title"] == Player_Name:
+        playlist_id = i["playlistId"]
+        break
+if playlist_id:
+    print("already exist")
+else:
+    playlist_id = yt.create_playlist(
+        title= Player_Name,
+        description=f"Playlist with the hottest songs from {date}",
+        privacy_status="PRIVATE",
+    )
+    print(f"Created playlist: {Player_Name}")
+
+# Search and add each song
+
+for song in song_names:
+    try:
+        searchs = yt.search(song,filter="songs")
+        video = searchs[0]["videoId"]
+        yt.add_playlist_items(playlist_id,[video])
+        print(f"Added : {song}")
+
+    except Exception as e:
+        print(f"Cannot add {song}")
+        print(e)
